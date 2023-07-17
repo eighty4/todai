14f4bc6eb6c5e326273e76771503f52983e1b93a
Adam McKee <adam.be.g84d@gmail.com>
2023-07-16 23:53:30 -0500

    ignore saved state when launching app with a debug build

--- lib/state.dart
+++ lib/state.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class TodaiAppState {
   static Future<bool> hasPassedIntro() async {
+    if (kDebugMode) {
+      if (!Platform.environment.containsKey('FLUTTER_TEST')) {
+        return false;
+      }
+    }
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool('intro') == true;
   }
